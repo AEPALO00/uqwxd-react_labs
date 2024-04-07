c2103bab69434ecba42731b8a26cd6639241b058
AEPALO00 <palomaresv.ae@gmail.com>
2024-04-07 16:33:26 -0400

    redux app

--- react-redux-master/src/actions/index.js
+++ react-redux-master/src/actions/index.js
@@ -1,1 +1,16 @@
 //actions
+const increment = (val) => {
+    return {
+        type : 'INCREMENT',
+        inc: val
+    }
+}
+
+const decrement = (val) => {
+    return {
+        type:'DECREMENT',
+        inc:val
+    }
+}
+
+export {increment, decrement};
